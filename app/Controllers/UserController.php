a13495b817c45f6c10822fd98fcd3a8d2e09642f
TUF <TUF@SOUNPRIL>
2026-03-13 14:56:54 +0700

    Soun

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+    require_once __DIR__."/../Models/User.php";
+
+    class UserController{
+        private $userModel;
+        public function __construct($db){
+            
+        }
+        {
+            throw new \Exception('Not implemented');
+        }
+    }
+?>
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,12 @@
         public function __construct($con){
             $this->db = $con;
         }
-        
+
+        public function selectUser($username){
+            $stmt = $this->db->prepare("SELECT * FROM users WHERE username = :u");
+            $stmt->bindParam(':u', $username);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC)?: false;
+        }
     }
 ?>
