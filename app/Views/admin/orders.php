5d15c818adcfaa1b9544c1c194b4895bc2b04fdd
TUF <TUF@SOUNPRIL>
2026-03-15 20:39:21 +0700

    Soun

--- app/Views/admin/orders.php
+++ app/Views/admin/orders.php
@@ -8,6 +8,7 @@
 </head>
 <body class="bg-gray-100 flex min-h-screen font-sans">
   <main class="">
+    <!-- Menu -->
     <nav class="flex-1 px-2 py-4 space-y-1">
       <a href="dashboard.php" class="flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-blue-600 font-semibold">
           <i class="fas fa-gauge-high w-5 text-center"></i> Dashboard
