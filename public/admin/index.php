4dbb0c703045b2f299ba02dad3c7b59f1dc1c0b3
soun-10 <sounpril168@gmail.com>
2026-04-14 11:42:07 +0700

    Soun

--- public/admin/index.php
+++ public/admin/index.php
@@ -33,7 +33,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Admin Login</title>
     <?php include __DIR__ ."/../../app/Views/components/cdns.php"; ?>
 </head>
 
